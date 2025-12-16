5905733a4da9cfd9e2e6ac83df0b73c317e52560
wangwt-spec <wangwenting83@gmail.com>
2025-12-16 17:39:21 +0900

    Create __init__.py

---
+++ src/identity_verifier/__init__.py
@@ -0,0 +1,15 @@
+"""
+identity_verifier
+
+A non-adjudicative library for assessing the public verifiability
+of claimed academic or professional identities.
+
+This package does NOT determine truth, fraud, or intent.
+"""
+
+from .core import assess_public_verifiability, EvidenceLevel
+
+__all__ = [
+    "assess_public_verifiability",
+    "EvidenceLevel",
+]
